c7cd67372e645da7b4f932d8cdb8351fe72e62d8
joanna <tuszynska.joanna@gmail.com>
2024-02-06 23:14:40 +0100

    set up for mistral instead of llama

--- index.ts
+++ index.ts
@@ -1,23 +1,18 @@
-import {API_KEY} from "./config";
-import {ChatOpenAI} from '@langchain/openai';
-import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import {HuggingFaceInference} from '@langchain/community/llms/hf';
+import {HUGGING_FACE_API_KEY} from "./config";
 
-(async() => {
-    const chat = new ChatOpenAI({
-        openAIApiKey: API_KEY,
-        modelName: 'gpt-4-turbo-preview',
-        })
-    const ans = await chat.invoke([
-        new SystemMessage('You are an ai which always answer with huge dose of humor an irony.'),
-        new HumanMessage('Hi! Who are you?'),
-    ], {
-        // functions: {
-        //     xyz: {
-        //         // możemy określić typ, parametry, itd
-        //     }
-        // },
+
+(async () => {
+    // Tworzymy model
+    const model = new HuggingFaceInference({
+        endpointUrl: 'https://nis5wwh2896n0io6.us-east-1.aws.endpoints.huggingface.cloud',
+        // model: 'meta-llama/Llama-2-70b-chat-hf',
+        apiKey: HUGGING_FACE_API_KEY,
     });
 
-    console.log(ans.additional_kwargs.function_call);
+    console.log('Asking ...');
+    const text = await model.invoke('What language model are you?');
+    console.log(text);
+
 
 })();
